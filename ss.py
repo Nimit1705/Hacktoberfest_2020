d2f5f3752a6a814a4f0c22461f8192f094fb053b
Dheeraj Mandvi <70431347+DheerajMandvi9@users.noreply.github.com>
2020-10-06 00:18:49 +0530

    Delete ss.py

--- ss.py
+++
@@ -1,4 +0,0 @@
-import mysql.connector
-mydb= mysql.connector.connect(host="localhost",user="dheeraj",passwd="mandvi",database="student")
-mycursor=mydb.cursor()
-mycursor.execute("CREATE TABLE student(Rollno int(3),Name varchar(15),Marks int(4))")
